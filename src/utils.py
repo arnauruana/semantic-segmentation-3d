0415aa905965e0b426da642810ace2aae90180a9
Arnau Ruana <arnau.ruana.i@estudiantat.upc.edu>
2023-07-18 13:55:15 +0200

    Add utils testing

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,7 @@ MAX_INT_VALUE = 2**31 - 1
 
 MIN_UINT_VALUE = 0
 MAX_UINT_VALUE = 2**32 - 1
+
+if __name__ == "__main__":
+    print(f"Int range: [{MIN_INT_VALUE}, {MAX_INT_VALUE}]")
+    print(f"Uint range: [{MIN_UINT_VALUE}, {MAX_UINT_VALUE}]")
